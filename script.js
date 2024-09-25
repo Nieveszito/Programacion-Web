44a47b6fa9f5e99add1b1d4fdfa1e6122f2fb4d5
Nieveszito <rnievess2200@alumno.ipn.mx>
2024-09-24 18:07:48 -0600

    Prograweb Nieves

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+//CLASE 24/09/24
+//NOMBRE: Nieves Sandoval Roberto Gawl
+
+
+//VARIABLES
+/*
+LAS VARIABLES GUARDAN INFORMACION QUE PUEDES REUTILIZAR 
+*/
+
+
+//Declaracion de variables 
+
+let nombre = 'Pedro'; //usa let para declarar variables que pueden cambiar su valor
+const PI = 3.1416;
+var edad = 25;
+
+//Operadores
+let a = 5;
+let b = 2;
+let suma = a + b ; //7
+let resta = a - b; //3
+let multiplicacion = a * b; //10
+let division = a/b; //2.5
+
+//Operadores de comparacion 
+a > b; //true
+a < b; //false 
+a == b; //false
+a === b; //false (compara valor y tipo)
+
+
 alert("hola, 5iv7 desde el js externo")
