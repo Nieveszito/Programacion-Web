3c24a687ded70bb4180e23bffcf5a56364ca5371
Nieveszito <rnievess2200@alumno.ipn.mx>
2024-10-10 16:48:24 -0600

    actualizacion

---
+++ 10102024/script.js
@@ -0,0 +1,12 @@
+//una linea
+/* comentarios
+multiples
+*/
+function hello(){
+    return 'hola js'
+
+}
+console.log(hello());
+/* inesvtigar funciones y ejemplificar cada una de ellas en JS
+con eso de parametros, funcion dentro de otra funcion.
+*/
