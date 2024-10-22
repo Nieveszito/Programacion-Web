2913c7712b0a568b8843eee02d8e28fec4e5aeee
Nieveszito <rnievess2200@alumno.ipn.mx>
2024-10-22 17:45:13 -0600

    clase 221024

---
+++ Clase 22_10_24/script.js
@@ -0,0 +1,16 @@
+function hola (a,b){
+    return a+b;
+}
+console.log(hola(3,5));
+
+//convertir funcion a flecha
+const suma = (a,b) => {
+    return a+b;
+}
+console.log(suma(3,5));
+
+const suma2 = (a,b) => a+b;
+console.log(suma(5,5));
+
+const  cuadrado = x => x*x;
+console.log(cuadrado(5));
